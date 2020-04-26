442e928a298605bf53a9296eae38a73e5669ce34
Harsha G <64349433+ghvardhan569@users.noreply.github.com>
2020-04-26 13:14:28 +0530

    Add files via upload

---
+++ video.py
@@ -0,0 +1,18 @@
+import pafy
+
+#enter the url of the video you want to download
+url="https://www.youtube.com/watch?v=ozTvBsrf1as"    # link of video url you want to download
+video=pafy.new(url)
+
+##get best resolution of the video
+best=video.getbest()
+
+###best resolution in required format[give your req format in "preftype"]
+best = video.getbest(preftype="mp4")
+
+
+#if you want to specify a particular path for the video
+filename = best.download(filepath="/Users/user/Desktop/")     # change the location as per your need . 
+
+
+## credit :- aihubprojects.com
